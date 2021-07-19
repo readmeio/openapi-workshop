84e90ba6cee5bc8dfde42cd26de765d2b3085599
Kelly Joseph Price <kelly@readme.com>
2021-07-19 15:14:00 -0700

    minor fix

--- exercises/titles/solution/solution.js
+++ exercises/titles/solution/solution.js
@@ -1,7 +1,6 @@
 const solution = userSpec => {
   const messages = [];
 
-  console.log(JSON.stringify({ userSpec }, null, 2));
   if (userSpec?.info?.title !== 'ReadMe Petstore') {
     messages.push(
       `'title' is wrong! You entered '${userSpec?.info?.title}', when it should have been 'ReadMe Petstore'`
